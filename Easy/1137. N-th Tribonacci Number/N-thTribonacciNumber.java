965bc1a348b8aef7cd4a4690d072f8f43546d559
edwu0029 <83135752+edwu0029@users.noreply.github.com>
2021-09-25 01:14:35 -0400

    Added N-th Tribonacci Number

---
+++ Easy/1137. N-th Tribonacci Number/N-thTribonacciNumber.java
@@ -0,0 +1,18 @@
+/*
+LeetCode Easy: 1137. N-th Tribonacci Number
+https://leetcode.com/problems/n-th-tribonacci-number/
+
+Check N-thTribonacciNumber.cpp for brief solution explanation.
+*/
+class Solution {
+    public int tribonacci(int n) {
+        int[]tribonacci = new int[Math.max(3, n+1)];
+        tribonacci[0] = 0;
+        tribonacci[1] = 1;
+        tribonacci[2] = 1;
+        for(int i = 3;i<=n;i++){
+            tribonacci[i] = tribonacci[i-1]+tribonacci[i-2]+tribonacci[i-3];
+        }
+        return tribonacci[n];
+    }
+}
