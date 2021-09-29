3d6f0af98ae83e13d1a7ab9dc0498fbe4170d7ec
edwu0029 <83135752+edwu0029@users.noreply.github.com>
2021-09-28 23:09:21 -0400

    Improved algorithm
    Change to a two pointer approach

--- Easy/922. Sort Array By Parity II/SortArrayByParityII.cpp
+++ Easy/922. Sort Array By Parity II/SortArrayByParityII.cpp
@@ -4,33 +4,35 @@ https://leetcode.com/problems/sort-array-by-parity-ii/
 
 Brief Solution Explanation:
 N = Length of nums
-- Simple implementation by collecting all even elements and odd elements
-- Then reconstruct the array with putting an element with the appropriate parity in
+- We can use a simple two pointer to solve this problem
+- Given that there are equal amounts of even and odd numbers, we know that for every
+misplaced even, there is also a misplaced odd (and vice versa)
+- Since the misplaced elements come in pairs, we can just use two pointers to continually find pairs
+of misplaced elements and swap them
+- Finally we can just return the nums array
 
 Time Complexity: O(N)
-Memory Complexity: O(N)
+Memory Complexity: O(1)
 */
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
         int N = nums.size();
-        vector<int>evens, odds;
-        for(int i = 0;i<N;i++){
-            if(nums[i]%2==0){
-                evens.push_back(nums[i]);
-            }else{
-                odds.push_back(nums[i]);
+        int i = 0, j = 1;
+        while(i<N&&j<N){
+            while(i<N&&nums[i]%2==0){
+                i+=2;
+            }
+            while(j<N&&nums[j]%2==1){
+                j+=2;
+            }
+            if(i<N&&j<N){
+                swap(nums[i], nums[j]);
+            }else if(i>=N||j>=N){ //One of our pointers has reached the end
+                //No more misplaced elements
+                break;
             }
         }
-        vector<int>ans;
-        int p1 = 0, p2 = 0;
-        for(int i = 0;i<N;i++){
-            if(i%2==0){
-                ans.push_back(evens[p1++]);
-            }else{
-                ans.push_back(odds[p2++]);
-            }
-        }
-        return ans;
+        return nums;
     }
 };
